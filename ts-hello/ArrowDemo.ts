aa49c2971352aaa6e2553e57bec75f7fcfa2612e
Pradeep Shettar <shettar20.as.developer@gmail.com>
2020-01-12 02:19:00 +0530

    Arrow Demo

---
+++ ts-hello/ArrowDemo.ts
@@ -0,0 +1,18 @@
+/*Common way of using a function in JS*/
+let logMessage = function(message) {
+    console.log(message);
+}
+
+/*Arrow Function in TS*/
+let Log = (message) => {
+    console.log(message);
+}
+
+/* 
+If the function is just one line, we need not include parenthesis.
+If the parameter is just one, we need not use parenthesis to the param list too.
+*/
+
+/*
+This is similar to lamda function in Java
+*/
